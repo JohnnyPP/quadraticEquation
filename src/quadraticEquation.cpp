2a2eb306cc45f463ef2fbc66f249165388a75651
JohnnyPP <johnnnyppp@gmail.com>
2015-06-04 13:47:20 +0200

    Initial release

---
+++ src/quadraticEquation.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : quadraticEquation.cpp
+// Author      : KK
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Search file" << endl; // prints Search file
+	return 0;
+}
